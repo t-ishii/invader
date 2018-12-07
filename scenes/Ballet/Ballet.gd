dd8e1e069f8e0e52e94c07f589ee9ce08526fd3c
t-ishii <perlispython.plp@gmail.com>
2018-12-08 00:28:24 +0900

    change node types

--- scenes/Ballet/Ballet.gd
+++ scenes/Ballet/Ballet.gd
@@ -1,13 +1,15 @@
-extends StaticBody2D
+extends Area2D
 
 var owner_name
+var regex
 
 func _process(delta):
     position.y -= delta * 100
 
 func _ready():
-    pass
-
+    regex = RegEx.new()
+    regex.compile('Enemy')
+    
 func _on_VisibilityNotifier2D_screen_exited():
     queue_free()
 
@@ -16,3 +18,9 @@ func set_owner_name(name):
 
 func get_owner_name():
     return owner_name
+
+func _on_Ballet_body_entered(body):
+    var result = regex.search(body.name)
+    if result:
+        body.get_node('AnimationPlayer').play('dead')
+        queue_free()
--- scenes/Enemy/Enemy.gd
+++ scenes/Enemy/Enemy.gd
@@ -1,12 +1,4 @@
-extends Area2D
-
-func _on_Enemy_body_entered(body):
-    if !body.has_method('get_owner_name'):
-        return
-
-    if body.get_owner_name() == 'Player':
-        body.queue_free()
-        $AnimationPlayer.play('dead')
+extends StaticBody2D
 
 func load_texture(texture):
     $Sprite.texture = load(texture)
@@ -15,4 +7,6 @@ func play_anime_by_direction(direction):
     var animation_type = 'left'
     if direction > 0:
         animation_type = 'right'
-    $AnimationPlayer.play(animation_type)
+
+    if $AnimationPlayer.current_animation != 'dead':
+        $AnimationPlayer.play(animation_type)
